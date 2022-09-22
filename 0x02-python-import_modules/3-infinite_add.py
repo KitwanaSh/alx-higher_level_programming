03810cde8cf4f5ed77616c05bb5cd9adea184607
KitwanaSh <rodriguekitweze@gmail.com>
2022-09-22 14:48:08 +0200

    Feat: add task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    num_args = 0
+    for index in range(len(sys.argv)- 1):
+        num_args = num_args + int(sys.argv[index + 1])
+    print("{}".format(num_args))
