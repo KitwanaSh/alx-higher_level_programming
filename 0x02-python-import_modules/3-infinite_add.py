4e58555d60ecaaf22a3add718a6ff1f45fb8456a
KitwanaSh <rodriguekitweze@gmail.com>
2022-09-22 14:56:24 +0200

    Fix: Update the task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
 
     num_args = 0
-    for index in range(len(sys.argv)- 1):
+    for index in range(len(sys.argv) - 1):
         num_args = num_args + int(sys.argv[index + 1])
     print("{}".format(num_args))
