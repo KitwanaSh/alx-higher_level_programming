e8ba2b5bda60af45ed85705b22e578c307462d6a
KitwanaSh <rodriguekitweze@gmail.com>
2022-09-28 19:12:46 +0200

    Fix: update

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,4 +3,4 @@ def uniq_add(my_list=[]):
     if not my_list:
         return None
     # return [set(x+y) for x in my_list for y in my_list]
-    return [x if x != x else sum(set(x)) for x in my_list]
+    return [sum(set(my_list)) for my_list in my_list] 
