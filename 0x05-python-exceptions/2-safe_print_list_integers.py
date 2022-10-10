fce264f1ec4b6a0204a97f892a095434767b648a
KitwanaSh <rodriguekitweze@gmail.com>
2022-10-10 16:45:08 +0200

    Feat: add task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    iterate_go = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]),  end="")
+        except (TypeError, ValueError):
+            pass
+        else:
+            iterate_go += 1
+    print()
+    return (iterate_go)
