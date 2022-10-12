05f1cf117e98718eb46e455009043028bb7e10da
KitwanaSh <rodriguekitweze@gmail.com>
2022-10-12 15:34:28 +0200

    Feat: add task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square class that defines a square"""
+
+class Square():
+    """ Class showing a square"""
+
+    def __init__(self, size):
+        """ initializing the class
+
+        Args:
+            size (int): square size
+        """
+        self.__size = size
