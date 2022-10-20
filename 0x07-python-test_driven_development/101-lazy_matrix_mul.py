4766cf89d0230e931977e7b265d91542076350a4
KitwanaSh <rodriguekitweze@gmail.com>
2022-10-20 13:33:45 +0200

    Feat: add the numpy task

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Bring the library (numpy) and use it for multiplication"""
+
+import numpy as np
+
+def lazy_matrix_mul(m_a, m_b):
+    """Multiplies two matrices using numpy
+    """
+
+
+    return np.matmul(m_a, m_b)
