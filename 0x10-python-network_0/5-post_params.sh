60aaf0f3687bb9a64ab92f706bb34e117af80eba
KitwanaSh <rodriguekitweze@gmail.com>
2023-01-16 20:57:11 +0200

    Fix: fix the post parm task

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a POST rquest to the pass URL
-curl -sX "$1" POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
