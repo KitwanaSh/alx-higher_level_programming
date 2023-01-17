7662afcc01be9c6c4380a909d84211641cfb455e
KitwanaSh <rodriguekitweze@gmail.com>
2023-01-17 14:39:11 +0200

    Feat: 'POST an email'

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Takes in a URL and an email, sends a POST request to the
+passed URL with the email as a paramater
+and displays the body i the response
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode('utf-8'))
