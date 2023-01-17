4af4f0fcdc3986f780947a5cbb0e676765a0f237
KitwanaSh <rodriguekitweze@gmail.com>
2023-01-17 16:04:11 +0200

    Feat: 'My Github'

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Uses the Github API to display a Github ID based on given credentials.
+using 3 arguments for run
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get("https://api.github.com/user", auth=auth)
+    print(req.json().get("id"))
